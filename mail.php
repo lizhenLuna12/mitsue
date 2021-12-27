f47e4ddfc5d54b233f2501a041466f5ebb7ff8d8
lizhenLuna12 <72483469+lizhenLuna12@users.noreply.github.com>
2021-12-27 13:40:17 +0900

    Update mail.php

--- mail.php
+++ mail.php
@@ -33,7 +33,7 @@ if (version_compare(PHP_VERSION, '5.1.0', '>=')) {//PHP5.1.0以上の場合の�
 $site_top = "http://www.php-factory.net/";
 
 //管理者のメールアドレス ※メールを受け取るメールアドレス(複数指定する場合は「,」で区切ってください 例 $to = "aa@aa.aa,bb@bb.bb";)
-$to = "xxxxxxxxxx@xxx.xxx";
+$to = "lizhen.training@gmail.com";
 
 //送信元メールアドレス（管理者宛て、及びユーザー宛メールの送信元メールアドレスです）
 //必ず実在するメールアドレスでかつ出来る限り設置先サイトのドメインと同じドメインのメールアドレスとすることを強く推奨します
